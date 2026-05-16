29f61b51c84e91e0c37370f969acc6b7da78d453
Matveyuchik <metrolastlightreduxing@gmail.com>
2026-05-16 22:25:13 +0300

    add: delete feature

--- fileditor.c
+++ fileditor.c
@@ -7,35 +7,54 @@
 
 int main (int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        printf("usage: fileditor <filename>");
+        printf("usage: fileditor <mode (w/d)> <filename>");
         return 1;
     }
 
-    const char* filename = argv[1];
+    const char* filename = argv[2];
     if (!filename) return 1;
 
-    FILE* file = fopen(filename, "w");
-    if (!file)
+    if (strcmp(argv[1], "w") == 0)
     {
-        printf("error: file cannot open :c\n");
-        return 1;
+
+        FILE* file = fopen(filename, "w");
+        if (!file)
+        {
+            printf("error: file cannot open :c\n");
+            return 1;
+        }
+
+        char buf[256];
+        if (!fgets(buf, sizeof(buf), stdin))
+        {
+            printf("error: cannot write string to buf\n");
+            return 1;
+        }
+        if (!fprintf(file, "%s", buf))
+        {
+            printf("error: cannot write to file %s\n", filename);
+            return 1;
+        }
+
+        printf("succesfully written to %s\n", filename);
+        fclose(file);
+        return 0;
     }
 
-    char buf[256];
-    if (!fgets(buf, sizeof(buf), stdin))
+    if (strcmp(argv[1], "d") == 0)
     {
-        printf("error: cannot write string to buf\n");
-        return 1;
-    }
-    if (!fprintf(file, "%s", buf))
-    {
-        printf("error: cannot write to file %s\n", filename);
-        return 1;
+        if (!remove(filename) != 0)
+        {
+            printf("error: cannot delete file :c\n");
+            return 1;
+        }
+
+        printf("succesfully deleted %s\n", filename);
+        return 0;
     }
 
-    printf("succesfully written to %s\n", filename);
-    fclose(file);
-    return 0;
+    printf("error: unknown type\n");
+    return 1;
 }
